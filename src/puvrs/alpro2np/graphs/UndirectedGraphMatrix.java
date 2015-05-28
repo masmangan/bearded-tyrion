641eb6e52eb32501ddeb495a280d7e74bd1e8119
Marco Aurelio Souza Mangan <10067320@l1818095.portoalegre.pucrsnet.br>
2015-05-27 22:38:55 -0300

    #1

---
+++ src/puvrs/alpro2np/graphs/UndirectedGraphMatrix.java
@@ -0,0 +1,31 @@
+package puvrs.alpro2np.graphs;
+
+import java.util.ArrayList;
+
+public class UndirectedGraphMatrix implements UndirectedGraph {
+
+	@Override
+	public void addVertice(String vertice) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEdge(String strOrig, String strDest) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getDegree(String vertice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<String> getAllAdjacents(String vertice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
