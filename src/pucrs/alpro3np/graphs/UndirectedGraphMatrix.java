8e4fbfcad66f2fa0658489383e8319c716a184f2
Marco Mangan (@FACIN 312) <marco.mangan@gmail.com>
2015-05-29 21:37:57 -0300

    esboco da classe

--- src/pucrs/alpro3np/graphs/UndirectedGraphMatrix.java
+++ src/pucrs/alpro3np/graphs/UndirectedGraphMatrix.java
@@ -1,31 +1,46 @@
 package pucrs.alpro3np.graphs;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UndirectedGraphMatrix implements UndirectedGraph {
 
+	private boolean[][] matrix;
+	private List<String> names;
+	
 	@Override
 	public void addVertice(String vertice) {
-		// TODO Auto-generated method stub
-
+		// TODO armazenar o vertice em names, se ainda não existir
 	}
 
 	@Override
 	public void addEdge(String strOrig, String strDest) {
-		// TODO Auto-generated method stub
-
+		// TODO obter posicao do strOrig
+		// TODO obter posicao do strDest
+		// TODO matrix[pos(strOrig)][pos(strDest)] = true;
+		// TODO matrix[pos(strDest)][pos(strOrig)] = true;
 	}
 
 	@Override
 	public int getDegree(String vertice) {
-		// TODO Auto-generated method stub
+		// TODO obter posicao do vertice
+		// TODO contar quantas saidas tem o vertice, na matrix
+		// TODO retornar o total
 		return 0;
 	}
 
 	@Override
 	public ArrayList<String> getAllAdjacents(String vertice) {
-		// TODO Auto-generated method stub
+		// TODO obter posicao do vertice
+		// TODO verificar quantas saidas tem o vertice, na matrix
+		// TODO para cada saida, armazenar o nome do vertice na resposta
+		// TODO retornar a resposta
 		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return names.toString() + matrix.toString();
 	}
 
 }
